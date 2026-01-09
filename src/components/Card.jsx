17f7306790f7b7570a7292be2917c3233e959605
Michela <micheladellagatta1@gmail.com>
2026-01-09 16:20:13 +0100

    Add Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card() {
+  return (
+    <>
+      <div className="dualia-card">
+        <img src="..." className="dualia-card-img" alt="..." />
+        <div className="dualia-card-body">
+          <h5 className="dualia-card-title">Card title</h5>
+          <p className="dualia-card-price">€99,99</p>
+        </div>
+      </div>
+    </>
+  );
+}
