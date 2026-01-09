4d5525a3d88ccba45399062b576ff889cdfad892
giannivitale <giavitale78@gmail.com>
2026-01-09 12:06:00 +0100

    jumbotron_intialize

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Jumbotron = ({title, subtile, imageSrc, imageAlt})
+
+return(
+    <div className='jumbotron position-relative overflow-hidden text-center bg-light'>
+
+<h1>Minimal Luxury for Inner Balance</h1>
+<h3>two colors one balance</h3>
+    </div>
+
+)
+
+export default Jumbotron
