c4d9804cbd8e4faafcc77a7cff0e6b93c8454f7b
Michela <micheladellagatta1@gmail.com>
2026-01-09 18:00:44 +0100

    Add path props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,20 @@
-export default function Card({ img, title, price }) {
+import { Link } from "react-router-dom";
+
+export default function Card({ img, title, price, path }) {
   return (
     <>
       <div className="dualia-card dualia-card-50 dualia-card-25 flex-grow-1">
-        <img src={img} className="dualia-card-img" alt={title} />
-        <div className="dualia-card-body">
-          <h5 className="dualia-card-title">{title}</h5>
-          <p className="dualia-card-price">{price}</p>
-        </div>
+        <Link
+          to={path}
+          className="m-0 p-0 text-decoration-none"
+          style={{ color: "inherit" }}
+        >
+          <img src={img} className="dualia-card-img" alt={title} />
+          <div className="dualia-card-body">
+            <h5 className="dualia-card-title">{title}</h5>
+            <p className="dualia-card-price">{price}</p>
+          </div>
+        </Link>
       </div>
     </>
   );
