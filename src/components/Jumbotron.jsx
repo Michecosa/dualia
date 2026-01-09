8a29b18b087e7b501d951c50cc92c18ccf12c2d4
giannivitale <giavitale78@gmail.com>
2026-01-09 19:18:05 +0100

    Update Jumbotron title and increase font size for better visibility

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -10,7 +10,7 @@ function Jumbotron() {
                 className="w-100 h-100 position-absolute top-0 start-0 object-fit-cover"
             />
             <div className="jumbotron-overlay position-relative p-5">
-                <h1 className="display-1 fw-bold text-white fs-1"> Minimal Luxury for Inner Balance </h1>
+                <h1 className="display-1 fw-bold text-white fs-1"> D U A L I A </h1>
                 <p className="lead text-white fs-5"> Two colors One balance </p>
             </div>
         </div>
