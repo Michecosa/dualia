99f18398b5541947542af0c4f13883a3fceb6deb
giannivitale <giavitale78@gmail.com>
2026-01-09 17:47:13 +0100

    Update title case in index.html, enhance Jumbotron text styles, and add newline in index.css

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -10,8 +10,8 @@ function Jumbotron() {
                 className="w-100 h-100 position-absolute top-0 start-0 object-fit-cover"
             />
             <div className="jumbotron-overlay position-relative p-5">
-                <h1 className="display-4 fw-bold text-white"> Minimal Luxury for Inner Balance </h1>
-                <p className="lead text-white"> Two colors One balance </p>
+                <h1 className="display-1 fw-bold text-white fs-1"> Minimal Luxury for Inner Balance </h1>
+                <p className="lead text-white fs-5"> Two colors One balance </p>
             </div>
         </div>
     );
