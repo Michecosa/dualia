4aadded4eaed65715a39f19d16b692266b7dca52
Michela <micheladellagatta1@gmail.com>
2026-01-09 17:53:40 +0100

    Fix media query for large screen

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ img, title, price }) {
   return (
     <>
-      <div className="dualia-card dualia-card-50 dualia-card-33 flex-grow-1">
+      <div className="dualia-card dualia-card-50 dualia-card-25 flex-grow-1">
         <img src={img} className="dualia-card-img" alt={title} />
         <div className="dualia-card-body">
           <h5 className="dualia-card-title">{title}</h5>
