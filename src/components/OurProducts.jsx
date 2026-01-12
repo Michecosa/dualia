10955b2302b18caaf4469b284ba56819be43578e
Michela <micheladellagatta1@gmail.com>
2026-01-12 18:10:55 +0100

    Update product details in OurProducts component

--- src/components/OurProducts.jsx
+++ src/components/OurProducts.jsx
@@ -10,10 +10,30 @@ export default function OurProducts() {
       <div className="product-container my-5">
         <h1 className="mb-3">Our Products</h1>
         <div className="row">
-          <Card img={img1} title="Cascading incense holder" price="€59,99" path="/product/cascading-incense-holder" />
-          <Card img={img2} title="Luxury ashtray" price="€999,99" path="/product/luxury-ashtray" />
-          <Card img={img3} title="Floor lamp" price="€199,99" path="/product/floor-lamp" />
-          <Card img={img4} title="Zen garden" price="€19,99" path="/product/zen-garden" />
+          <Card
+            img={img1}
+            title="Incense Burner black"
+            price="€157,99"
+            path="/products/incense-burner-black"
+          />
+          <Card
+            img={img2}
+            title="Ashtray Black"
+            price="814,99"
+            path="/products/ashtray-black"
+          />
+          <Card
+            img={img3}
+            title="Floor Lamp black"
+            price="€1119,99"
+            path="/products/floor-lamp-black"
+          />
+          <Card
+            img={img4}
+            title="Zen Garden black"
+            price="€69,99"
+            path="/products/zen-garden-black"
+          />
         </div>
         <div className="d-flex justify-content-center">
           <button className="btn btn-dualia-dark">View all</button>
