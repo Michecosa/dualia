62ad5be5cb9f248f39b4971b6a904b3149ab86c6
akthernahela <nahela00@gmail.com>
2026-01-12 10:46:45 +0100

    Add about us to the home page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import OurProducts from "../components/OurProducts";
 import Jumbotron from "../components/Jumbotron";
+import AboutUs from "../components/AboutUs";
 export default function Homepage() {
   return (
     <>
       <div>
         <Jumbotron />
         <OurProducts />
+        <AboutUs />
       </div>
     </>
   );
