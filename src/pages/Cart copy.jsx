3064944e0d235c6d79accf5d3195a430bb9ccca5
gilli-jonathan <gilli.jonathan97@gmail.com>
2026-01-13 13:02:48 +0100

    bases cart

---
+++ src/pages/Cart copy.jsx
@@ -0,0 +1,62 @@
+export default function Cart() {
+
+    return (
+        <section id="shopping_cart" className="my-4 container">
+
+            <div className="row">
+                <div className="col-md-8 cart">
+                    <div className="title">
+                        <div className="row">
+                            <div className="col py-3">
+                                <h4><b>Shopping Cart</b></h4>
+                            </div>
+                            <div className="">3 items</div>
+                        </div>
+                    </div>
+
+
+                    <div className="row border-top border-bottom">
+                        <div className="row main align-items-center">
+                            <div className="col-2"><img className="img-fluid" src="https://i.imgur.com/1GrakTl.jpg" /></div>
+                            <div className="col">
+                                <div className="row">nome prodotto</div>
+                            </div>
+                            <div className="col">
+                                <a href="#">-</a><a href="#" className="border">1</a><a href="#">+</a>
+                            </div>
+                            <div className="col">44.00 <span className="close">&#10005;</span></div>
+                        </div>
+                    </div>
+
+
+
+                    <div className="back-to-shop"><a href="#">freccia</a><span className="text-muted">Back to shop</span></div>
+                </div>
+
+                {/* lato pagamento */}
+                <div className="col-md-4 summary">
+                    <div><h5><b>Summary</b></h5></div>
+                    <hr />
+                    <div className="row">
+                        <div className="col">ITEMS 3</div>
+                        <div className="col text-right">&euro; 132.00</div>
+                    </div>
+                    <form>
+                        <p>SHIPPING</p>
+                        <select><option className="text-muted">Standard-Delivery- &euro;5.00</option></select>
+                        <p>GIVE CODE</p>
+                        <input id="code" placeholder="Enter your code" />
+                    </form>
+                    <div className="row">
+                        <div className="col">TOTAL PRICE</div>
+                        <div className="col text-right">&euro; 137.00</div>
+                    </div>
+                    <button className="btn">CHECKOUT</button>
+                </div>
+            </div>
+
+
+        </section>
+    )
+
+}
