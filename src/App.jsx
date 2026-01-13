17cd8eaac592306ec44b14f51592e2d8c1330178
gilli-jonathan <gilli.jonathan97@gmail.com>
2026-01-13 11:51:05 +0100

    create pages, add navlink in header

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import DefaultLayout from "./layout/DefaultLayout";
 import Homepage from "./pages/Homepage";
 import Products from "./pages/Products";
 import ProductDetail from "./pages/ProductDetail";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Homepage />}></Route>
           <Route path="/products" element={<Products />}></Route>
           <Route path="/products/:id" element={<ProductDetail />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
