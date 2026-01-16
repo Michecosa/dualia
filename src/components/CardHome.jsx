5d955929eb587dc9094f0f63ee5431ad26875d89
gilli-jonathan <gilli.jonathan97@gmail.com>
2026-01-16 14:32:53 +0100

    aggiunto il componente card home e applicato in OurProducts.jsx

---
+++ src/components/CardHome.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+export default function CardHome({
+    img,
+    title,
+    price,
+    path,
+    bestSellers,
+    prodRes,
+    fullprice,
+}) {
+    return (
+        <>
+            <div
+                className={`dualia-card dualia-card-50 dualia-card-25 ${bestSellers} ${prodRes} flex-grow-1 mb-4`}>
+                <Link
+                    to={path}
+                    className="m-0 p-0 text-decoration-none"
+                    style={{ color: "inherit" }}
+                >
+                    <img src={img} className="dualia-card-img" alt={title} />
+                    <div className="dualia-card-body mt-2">
+                        <h5 className="dualia-card-title">{title}</h5>
+                        <p className="dualia-card-price">
+                            {price < fullprice ? (
+                                <>
+                                    <strong id="scontato">{price}€</strong>
+                                    <s className="ms-3">{fullprice}€</s>
+                                </>
+                            ) : (
+                                price + "€"
+                            )}
+                        </p>
+                    </div>
+                </Link>
+            </div>
+        </>
+    );
+}
--- src/components/OurProducts.jsx
+++ src/components/OurProducts.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Card from "./Card";
+import CardHome from "./CardHome";
 import img1 from "../assets/immagini_dualia/porta_incenso_black.jpeg";
 import img2 from "../assets/immagini_dualia/luxury_ashtray_black.jpeg";
 import img3 from "../assets/immagini_dualia/floor_lamp_black.jpeg";
@@ -11,25 +11,25 @@ export default function OurProducts() {
       <div className="product-container my-5">
         <h1 className="mb-3">Our Products</h1>
         <div className="row">
-          <Card
+          <CardHome
             img={img1}
             title="Incense Burner black"
             price="157.99"
             path="/products/incense-burner-black"
           />
-          <Card
+          <CardHome
             img={img2}
             title="Ashtray Black"
             price="814.99"
             path="/products/ashtray-black"
           />
-          <Card
+          <CardHome
             img={img3}
             title="Floor Lamp black"
             price="1119.99"
             path="/products/floor-lamp-black"
           />
-          <Card
+          <CardHome
             img={img4}
             title="Zen Garden black"
             price="69.99"
