0c8b007dcda67df432f3313e6f5a527f3b659a9f
gilli-jonathan <gilli.jonathan97@gmail.com>
2026-01-20 11:59:24 +0100

    change Jumbotron img

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import jumbotronImage from '../assets/Jumbotron_photo.jpeg';
+import jumbotronImage from '../assets/Jumbotron_photo.png';
 
 function Jumbotron() {
     return (
@@ -10,7 +10,7 @@ function Jumbotron() {
                 className="w-100 h-100 position-absolute top-0 start-0 object-fit-cover"
             />
             <div className="jumbotron-overlay position-relative p-5">
-                <h1 className="display-1 fw-bold text-white fs-1"> D U A L I A </h1>
+                <h1 className="display-1 fw-bold text-white fs-1 pt-4"> D U A L I A </h1>
                 <p className="lead text-white fs-5"> Two colors One balance </p>
                 <div className="container text-center p-5 mt-5">
                     <p className='lead text-white fs-5'>
