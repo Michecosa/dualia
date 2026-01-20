133638f111890966cb610c281321835fabd7f8d0
VincenzoLivigni <enzolivigni13@gmail.com>
2026-01-20 11:22:21 +0100

    fix logoAnimation

--- src/components/LogoAnimation.jsx
+++ src/components/LogoAnimation.jsx
@@ -1,14 +1,17 @@
-import cerchio from "../assets/Cerchio.png"
-import cornice from "../assets/Cornice.png"
+import cerchio from "../assets/logo-oro.png"
+import cornice from "../assets/cornice-oro.png"
 
 export default function LogoAnimation() {
 
     return (
 
         <>
-            <div className="logo-animation">
-                <img id="cornice" src={cornice} alt="" />
-                <img id="cerchio" src={cerchio} alt="" />
+            <div className="vh-100 w-100 bg-black d-flex align-items-center justify-content-center flex-column" style={{ opacity: "0.75" }}>
+                <div className="logo-animation">
+                    <img id="cornice" src={cornice} alt="" className="" />
+                    <img id="cerchio" src={cerchio} alt="" className="" />
+                </div>
+                <h5 className="text-center mt-3" style={{ color: "#e3be85" }}>Payment in progress...</h5>
             </div>
 
         </>
